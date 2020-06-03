35eaf4fe4b5d5dba831151d53a4c53e8f91fb2ea
portsoft3710 <47545184+portsoft3710@users.noreply.github.com>
2020-06-03 22:28:34 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,38 @@
 // service-worker.js
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
+var CACHE_NAME = 'pwa-janken-2020060301';
+var urlsToCache = [
+    'index.html',
+    'ok.html',
+    'kokan_thankyou.html',    
+    '/css/style.css',
+    '/css/messageBox.css'
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+        .open(CACHE_NAME)
+        .then(function(cache){
+            return cache.addAll(urlsToCache);
+        })
+    );
 });
+
 
 self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
 });
 
 // 現状では、この処理を書かないとService Workerが有効と判定されないようです
-self.addEventListener('fetch', function(event) {});
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                if(response){
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
