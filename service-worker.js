cdaa6a45b9cc7b9d86132a455506fc92cafa9fdd
portsoft3710 <47545184+portsoft3710@users.noreply.github.com>
2020-06-03 23:43:10 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,21 @@
 // service-worker.js
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
+var CACHE_NAME = 'janken-cache-2020060305';
+var urlsToCache = [
+    'index.html',
+    'ok.html',
+    'kokan_thankyou.html',
+    './css/style.css',
+    './css/messageBox.css'
+];
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache.map(url => new Request(url, {credentials: 'same-origin'})));
+      })
+  );
 });
 
 self.addEventListener('activate', function(e) {
