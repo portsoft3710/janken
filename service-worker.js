3239e6e304595494581906afd73d980d52537eb9
portsoft3710 <47545184+portsoft3710@users.noreply.github.com>
2020-06-05 16:50:29 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -24,3 +24,17 @@ self.addEventListener('activate', function(e) {
 
 // 現状では、この処理を書かないとService Workerが有効と判定されないようです
 self.addEventListener('fetch', function(event) {});
+
+self.addEventListener('push', function (event) {
+    console.log('Received a push message', event);
+    var title = "プッシュ通知です！";
+    var body = "プッシュ通知はこのようにして送られるのです";
+
+    event.waitUntil(
+        self.registration.showNotification(title, {
+            body: body,
+            icon: 'images/icons/icon-152x152.png',
+            tag: 'push-notification-tag'
+        })
+    );
+});
